1a10b344338752272d98fb3bbe8fe926d6b186e4
Arlei Silva <arleilps@gmail.com>
2015-02-10 12:31:28 -0800

    Working on scripts for spatial compression.

--- experiments/split_train_test/split.sh
+++ experiments/split_train_test/split.sh
@@ -1,7 +1,10 @@
 input_file_name=$1
 output_file_name=$2
 size_train=(5 10 20 40)
-num_lines=`wc -l $input_file_name | awk '{print $1}'`
+
+sort -nk3,3 -t, $input_file_name > sorted.tmp
+
+num_lines=`wc -l sorted.tmp | awk '{print $1}'`
 e=`echo "scale=10; $num_lines+1" | bc`
 
 for s in ${size_train[@]}
@@ -9,7 +12,9 @@ do
   t=`echo "scale=10; $num_lines*$s/100" | bc`
   t=`echo "$t" | python -c "print int(round(float(raw_input())))"`
   t=`echo "scale=10; $t+1" | bc`
-  sed -n 2,$t\p $input_file_name > $output_file_name\_train_$s
+  sed -n 2,$t\p sorted.tmp > $output_file_name\_train_$s
   t=`echo "scale=10; $t+1" | bc`
-  sed -n $t,$e\p $input_file_name > $output_file_name\_test_$s
+  sed -n $t,$e\p sorted.tmp > $output_file_name\_test_$s
 done
+
+rm sorted.tmp
