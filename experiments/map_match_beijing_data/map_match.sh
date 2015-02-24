20dd553d9a3f7f63b1d057aa0caa04cd2597b56e
Arlei Silva <arleilps@gmail.com>
2015-02-24 13:33:22 -0800

    Adding script to map-match beijing data.

---
+++ experiments/map_match_beijing_data/map_match.sh
@@ -0,0 +1,1 @@
+../../traj_comp -g ../../../data/road_net_beijing.csv -o ../../../data/map_matched_cab_stream_beijing.txt -c MAP -d ../../../data/cab_stream_beijing.txt -n 10
--- experiments/map_match_sfo_data/map_match.sh
+++ experiments/map_match_sfo_data/map_match.sh
@@ -1,1 +1,1 @@
-../../traj_comp -g ../../road_net_sfo.csv -o ../../../data/map_matched_cab_stream_sfo.txt -c MAP -d ../../../data/cab_stream_sfo.txt -n 6
+../../traj_comp -g ../../../data/road_net_sfo.csv -o ../../../data/map_matched_cab_stream_sfo.txt -c MAP -d ../../../data/cab_stream_sfo.txt -n 6
