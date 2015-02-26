bafd3b430ef3096f50d8708abfe4d714bbe42341
Arlei Silva <arleilps@gmail.com>
2015-02-26 15:13:59 -0800

    Spliting by trajectory not update.

--- experiments/split_train_test/split.sh
+++ experiments/split_train_test/split.sh
@@ -1,20 +1,24 @@
 input_file_name=$1
 output_file_name=$2
-num_folds=10
+num_folds=3
 
-num_lines=`wc -l $input_file_name | awk '{print $1}'`
+cut -f1,1 -d, $input_file_name | sort -k1,1 -t, -u > traj.tmp
+num_traj=`wc -l traj.tmp | awk '{print $1}'`
 start="1"
-fold_size=`echo "scale=10; $num_lines/$num_folds" | bc`
+fold_size=`echo "scale=10; $num_traj/$num_folds" | bc`
 fold_size=`echo "$fold_size" | python -c "print int(round(float(raw_input())))"`
 end=$fold_size
+
 for ((s=1; s<=$num_folds;s++))
 do
-  sed -n $start,$end\p $input_file_name > fold_$s
+  sed -n $start,$end\p traj.tmp > fold_$s
   start=`echo "scale=1; $end+1" | bc`
   start=`echo "$start" | python -c "print int(round(float(raw_input())))"`
   end=`echo "scale=1; $end+$fold_size" | bc`
   end=`echo "$end" | python -c "print int(round(float(raw_input())))"`
 done
+
+rm traj.tmp
 
 for ((s=1; s<=$num_folds;s++))
 do
@@ -25,9 +29,9 @@ do
 	do
 		if [ $s -eq $i ] 
 		then
-			cat fold_$i > $output_file_name\_test_$s
+			grep -f fold_$i $input_file_name > $output_file_name\_test_$s
 		else
-			cat fold_$i >> $output_file_name\_train_$s
+			grep -f fold_$i $input_file_name >> $output_file_name\_train_$s
 		fi
 	done
 done
