f9d187aafbdc4ca58b63c436ffe1ae0a17cb68fc
Arlei Silva <arleilps@gmail.com>
2015-02-26 18:26:14 -0800

    Fixing.

--- experiments/spatial_compression/run_short_path_freq_subt.sh
+++ experiments/spatial_compression/run_short_path_freq_subt.sh
@@ -10,11 +10,11 @@ for u in ${length_subt_vec[@]}
 do
 	for ((f=1; f<=$num_folds;f++))
 	do
-		echo "$traj_comp -c SPFS -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_train_$f -p $short_path -s $min_sup -u $u -n $n_threads > SPFS/train_train_$f\_$u"
-		$traj_comp -c SPFS -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_train_$f -p $short_path -s $min_sup -u $u -n $n_threads > SPFS/train_train_$f\_$u
+		echo "$traj_comp -c SPFS -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_train_$f -p $short_path -s $min_sup -u $u -f $conf_file > SPFS/train_train_$f\_$u"
+		$traj_comp -c SPFS -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_train_$f -p $short_path -s $min_sup -u $u -f $conf_file > SPFS/train_train_$f\_$u
 	
-		echo "$traj_comp -c SPFS -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_test_$f -p $short_path -s $min_sup -u $u -n $n_threads > SPFS/train_test_$f\_$u"
-		$traj_comp -c SPFS -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_test_$f -p $short_path -s $min_sup -u $u -n $n_threads > SPFS/train_test_$f\_$u
+		echo "$traj_comp -c SPFS -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_test_$f -p $short_path -s $min_sup -u $u -f $conf_file > SPFS/train_test_$f\_$u"
+		$traj_comp -c SPFS -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_test_$f -p $short_path -s $min_sup -u $u -f $conf_file > SPFS/train_test_$f\_$u
 	done
 done
 
