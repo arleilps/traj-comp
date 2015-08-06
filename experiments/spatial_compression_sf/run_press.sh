d2a82a46e4bbbfdbcd51934adee4b20159d9f7f8
Arlei Silva <arleilps@gmail.com>
2015-08-05 23:56:06 -0700

    Fixing script

--- experiments/spatial_compression_sf/run_press.sh
+++ experiments/spatial_compression_sf/run_press.sh
@@ -13,7 +13,7 @@ for a in ${delay_vec[@]}
 do
 	for ((f=1; f<=$num_folds;f++))
 	do
-		echo "$traj_comp -c SPFS -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_test_$f -p $short_path -s $min_sup -u $length_subt_vec -f $conf_file -a $a -n $n_threads > SPFS/train_test_$f\_$a"
-		$traj_comp -c SPFS -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_test_$f -p $short_path -s $min_sup -u $length_subt_vec -f $conf_file -a $a -n $n_threads > SPFS/train_test_$f\_$a
+		echo "$traj_comp -c SPFS -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_test_$f -p $short_path -s $min_sup -u $length_subt_vec -f $conf_file -a $a -n $n_threads -l $short_path_file > SPFS/train_test_$f\_$a"
+		$traj_comp -c SPFS -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_test_$f -p $short_path -s $min_sup -u $length_subt_vec -f $conf_file -a $a -n $n_threads -l $short_path_file > SPFS/train_test_$f\_$a
 	done
 done
