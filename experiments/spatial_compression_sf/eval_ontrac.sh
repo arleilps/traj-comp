4121b850a94750df98c6926fb056aa394e87eae1
Arlei Silva <arleilps@gmail.com>
2015-08-07 19:45:12 -0700

    Updating scripts.

--- experiments/spatial_compression_sf/eval_ontrac.sh
+++ experiments/spatial_compression_sf/eval_ontrac.sh
@@ -14,57 +14,41 @@ extract (){
 	echo $value
 }
 
-results_file_name='PPM_order.dat'
+results_file_name='ontrac_time.dat'
 rm $results_file_name
 
 for d in ${order_vec[@]}
 do
-	avg_comp_ratio_train_train=0
 	avg_comp_ratio_train_test=0
-	avg_comp_time_train_train=0
 	avg_comp_time_train_test=0
-	avg_train_time_train_train=0
 	avg_train_time_train_test=0
 
 	for ((f=1; f<=$num_folds;f++))
 	do
-#		comp_ratio_train_train=$(extract "PPM/train_train_$f\_$d" "compression_ratio")
-#		avg_comp_ratio_train_train=`echo "scale=10; $avg_comp_ratio_train_train+$comp_ratio_train_train" | bc`
-		
 		comp_ratio_train_test=$(extract "PPM/train_test_${f}_${d}" "compression_ratio")
 		avg_comp_ratio_train_test=`echo "scale=10; $avg_comp_ratio_train_test+$comp_ratio_train_test" | bc`
-		
-#		comp_time_train_train=$(extract "PPM/train_train_$f\_$d" "compression_time")
-#		avg_comp_time_train_train=`echo "scale=10; $avg_comp_time_train_train+$comp_time_train_train" | bc`
 		
 		comp_time_train_test=$(extract "PPM/train_test_${f}_${d}" "compression_time")
 		avg_comp_time_train_test=`echo "scale=10; $avg_comp_time_train_test+$comp_time_train_test" | bc`
 
-#		train_time_train_train=$(extract "PPM/train_train_$f\_$d" "training_time")
-#		avg_train_time_train_train=`echo "scale=10; $avg_comp_time_train_train+$train_time_train_train" | bc`
-		
 		train_time_train_test=$(extract "PPM/train_test_${f}_${d}" "training_time")
 		avg_train_time_train_test=`echo "scale=10; $avg_train_time_train_test+$train_time_train_test" | bc`
 	done
 	
-#	avg_comp_ratio_train_train=`echo "scale=10; $avg_comp_ratio_train_train/$num_folds" | bc`
 	avg_comp_ratio_train_test=`echo "scale=10; $avg_comp_ratio_train_test/$num_folds" | bc`
-	
-#	avg_comp_time_train_train=`echo "scale=10; $avg_comp_time_train_train/$num_folds" | bc`
 	avg_comp_time_train_test=`echo "scale=10; $avg_comp_time_train_test/$num_folds" | bc`
-	
 	avg_train_time_train_train=`echo "scale=10; $avg_train_time_train_train/$num_folds" | bc`
-#	avg_train_time_train_test=`echo "scale=10; $avg_train_time_train_test/$num_folds" | bc`
-
-	echo "$d	$avg_comp_ratio_train_test	$avg_comp_time_train_test	$avg_train_time_train_test" >> $results_file_name
 done
 
-results_file_name='PPM_delay.dat'
+echo "$d        $avg_train_time_train_test" >> $results_file_name
+echo "$d        $avg_comp_time_train_test" >> $results_file_name
+
+results_file_name='ontrac_delay.dat'
 rm $results_file_name
 
 for a in ${delay_vec[@]}
 do
 	delay=`echo "scale=1; $a/60" | bc`
-	echo "$delay	$avg_comp_ratio_train_test	$avg_comp_time_train_test	$avg_train_time_train_test" >> $results_file_name
+	echo "$delay	$avg_comp_ratio_train_test" >> $results_file_name
 done
 
--- experiments/spatial_compression_sf/eval_press.sh
+++ experiments/spatial_compression_sf/eval_press.sh
@@ -14,47 +14,38 @@ extract (){
 	echo $value
 }
 
-results_file_name='short_path_freq_subt_length_subt_delay.dat'
+results_file_name='press_delay.dat'
 rm $results_file_name
 
 for a in ${delay_vec[@]}
 do
-	for u in ${length_subt_vec[@]}
+	avg_comp_ratio_train_test=0
+	avg_comp_time_train_test=0
+	avg_train_time_train_test=0
+
+	for ((f=1; f<=$num_folds;f++))
 	do
-		avg_comp_ratio_train_test=0
-		avg_comp_time_train_test=0
-		avg_train_time_train_test=0
-
-		for ((f=1; f<=$num_folds;f++))
-		do
-			comp_ratio_train_test=$(extract "SPFS/train_test_${f}_${u}_${a}" "compression_ratio")
-			avg_comp_ratio_train_test=`echo "scale=10; $avg_comp_ratio_train_test+$comp_ratio_train_test" | bc`
+		comp_ratio_train_test=$(extract "SPFS/train_test_${f}_${a}" "compression_ratio")
+		avg_comp_ratio_train_test=`echo "scale=10; $avg_comp_ratio_train_test+$comp_ratio_train_test" | bc`
 		
-			comp_time_train_test=$(extract "SPFS/train_test_${f}_${u}_${a}" "compression_time")
-			avg_comp_time_train_test=`echo "scale=10; $avg_comp_time_train_test+$comp_time_train_test" | bc`
+		comp_time_train_test=$(extract "SPFS/train_test_${f}_${a}" "compression_time")
+		avg_comp_time_train_test=`echo "scale=10; $avg_comp_time_train_test+$comp_time_train_test" | bc`
 
-			train_time_train_test=$(extract "SPFS/train_test_${f}_${u}_${a}" "training_time")
-			avg_train_time_train_test=`echo "scale=10; $avg_train_time_train_test+$train_time_train_test" | bc`
-		done
-	
-#		avg_comp_ratio_train_train=`echo "scale=10; $avg_comp_ratio_train_train/$num_folds" | bc`
-		avg_comp_ratio_train_test=`echo "scale=10; $avg_comp_ratio_train_test/$num_folds" | bc`
-	
-#		avg_comp_time_train_train=`echo "scale=10; $avg_comp_time_train_train/$num_folds" | bc`
-		avg_comp_time_train_test=`echo "scale=10; $avg_comp_time_train_test/$num_folds" | bc`
-	
-#		avg_train_time_train_train=`echo "scale=10; $avg_train_time_train_train/$num_folds" | bc`
-		avg_train_time_train_test=`echo "scale=10; $avg_train_time_train_test/$num_folds" | bc`
-		delay=`echo "scale=1; $a/60" | bc`
-		echo "$delay	$avg_comp_ratio_train_test	$avg_comp_time_train_test	$avg_train_time_train_test" >> $results_file_name
+		train_time_train_test=$(extract "SPFS/train_test_${f}_${a}" "training_time")
+		avg_train_time_train_test=`echo "scale=10; $avg_train_time_train_test+$train_time_train_test" | bc`
 	done
+	
+	avg_comp_ratio_train_test=`echo "scale=10; $avg_comp_ratio_train_test/$num_folds" | bc`
+	avg_comp_time_train_test=`echo "scale=10; $avg_comp_time_train_test/$num_folds" | bc`
+	avg_train_time_train_test=`echo "scale=10; $avg_train_time_train_test/$num_folds" | bc`
+	
+	delay=`echo "scale=1; $a/60" | bc`
+	
+	echo "$delay	$avg_comp_ratio_train_test" >> $results_file_name
 done
 
-results_file_name='short_path_freq_subt_length_subt_order.dat'
+results_file_name='press_time.dat'
 rm $results_file_name
 
-for d in ${order_vec[@]}
-do
-	echo "$d	$avg_comp_ratio_train_test	$avg_comp_time_train_test	$avg_train_time_train_test" >> $results_file_name
-done
-
+echo "$d	$avg_train_time_train_test" >> $results_file_name
+echo "$d	$avg_comp_time_train_test" >> $results_file_name
