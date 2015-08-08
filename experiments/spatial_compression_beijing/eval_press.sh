f4f9d81c6fc9a93eb66be198ceb42e7b23925b4d
Arlei Silva <arleilps@gmail.com>
2015-08-07 20:12:42 -0700

    -

--- experiments/spatial_compression_beijing/eval_press.sh
+++ experiments/spatial_compression_beijing/eval_press.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#       Evaluates PPM compression
+#       Evaluates PRESS compression
 #       @Arlei Silva
 
 source settings.sh
@@ -14,10 +14,10 @@ extract (){
 	echo $value
 }
 
-results_file_name='ontrac_time.dat'
+results_file_name='press_delay.dat'
 rm $results_file_name
 
-for d in ${order_vec[@]}
+for a in ${delay_vec[@]}
 do
 	avg_comp_ratio_train_test=0
 	avg_comp_time_train_test=0
@@ -25,30 +25,27 @@ do
 
 	for ((f=1; f<=$num_folds;f++))
 	do
-		comp_ratio_train_test=$(extract "PPM/train_test_${f}_${d}" "compression_ratio")
+		comp_ratio_train_test=$(extract "SPFS/train_test_${f}_${a}" "compression_ratio")
 		avg_comp_ratio_train_test=`echo "scale=10; $avg_comp_ratio_train_test+$comp_ratio_train_test" | bc`
 		
-		comp_time_train_test=$(extract "PPM/train_test_${f}_${d}" "compression_time")
+		comp_time_train_test=$(extract "SPFS/train_test_${f}_${a}" "compression_time")
 		avg_comp_time_train_test=`echo "scale=10; $avg_comp_time_train_test+$comp_time_train_test" | bc`
 
-		train_time_train_test=$(extract "PPM/train_test_${f}_${d}" "training_time")
+		train_time_train_test=$(extract "SPFS/train_test_${f}_${a}" "training_time")
 		avg_train_time_train_test=`echo "scale=10; $avg_train_time_train_test+$train_time_train_test" | bc`
 	done
 	
 	avg_comp_ratio_train_test=`echo "scale=10; $avg_comp_ratio_train_test/$num_folds" | bc`
 	avg_comp_time_train_test=`echo "scale=10; $avg_comp_time_train_test/$num_folds" | bc`
 	avg_train_time_train_test=`echo "scale=10; $avg_train_time_train_test/$num_folds" | bc`
-done
-
-echo "0        $avg_train_time_train_test" >> $results_file_name
-echo "0        $avg_comp_time_train_test" >> $results_file_name
-
-results_file_name='ontrac_delay.dat'
-rm $results_file_name
-
-for a in ${delay_vec[@]}
-do
+	
 	delay=`echo "scale=1; $a/60" | bc`
+	
 	echo "$delay	$avg_comp_ratio_train_test" >> $results_file_name
 done
 
+results_file_name='press_time.dat'
+rm $results_file_name
+
+echo "0	$avg_train_time_train_test" >> $results_file_name
+echo "0	$avg_comp_time_train_test" >> $results_file_name
--- experiments/spatial_compression_sf/plot_comp_ratio_delay.gp
+++ experiments/spatial_compression_sf/plot_comp_ratio_delay.gp
@@ -3,7 +3,7 @@ set style fill solid 1.00 border
 set style data histogram
 set style histogram cluster gap 1
 set encoding iso_8859_1
-set output "comp_ratio_delay.eps"
+set output "comp_ratio_delay_sf.eps"
 set xlabel "delay (min)"
 set ylabel "compression ratio"
 set key top left
@@ -11,5 +11,5 @@ set ytics 5
 set yrange [0:19]
 set logscale x 2
 set boxwidth .9 absolute
-plot "PPM_delay.dat" using 1:2 title "ONTRAC" with linespoints lt 1 lc 1 lw 4 pt 2 ps 3, "short_path_freq_subt_length_subt_delay.dat" using 1:2 title "PRESS" with linespoints lt 1 lc 3 lw 4 pt 7 ps 3
+plot "ontrac_delay.dat" using 1:2 title "ONTRAC" with linespoints lt 1 lc 1 lw 4 pt 2 ps 3, "press_delay.dat" using 1:2 title "PRESS" with linespoints lt 1 lc 3 lw 4 pt 7 ps 3
 
