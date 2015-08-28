ceb35cdfcc2d84273ce9df36a9f5135a8a4c6647
Arlei Silva <arleilps@gmail.com>
2015-08-28 07:55:54 -0700

    -

--- experiments/querying_sf/run_ontrac_part.sh
+++ experiments/querying_sf/run_ontrac_part.sh
@@ -10,7 +10,7 @@ for a in ${delay_vec[@]}
 do
 	for ((f=1; f<=$num_folds;f++))
 	do
-		echo "./traj_comp -g $roat_net -t $map_matched_traj\_train_$ -e $map_matched_traj\_test_$f -c ONTRAC-FULL -r 2 -f ../data/sfo_postgis.conf -m $a -n $num_threads -o ouput_$f\_$a -i $num_iter -q $queries\_test_$f"
-		./traj_comp -g $roat_net -t $map_matched_traj\_train_$ -e $map_matched_traj\_test_$f -c ONTRAC-FULL -r 2 -f ../data/sfo_postgis.conf -m $a -n $num_threads -o ouput_$f\_$a -i $num_iter -q $queries\_test_$f
+		echo "$traj_comp -g $roat_net -t $map_matched_traj\_train_$ -e $map_matched_traj\_test_$f -c ONTRAC-FULL -r 2 -f ../data/sfo_postgis.conf -m $a -n $num_threads -o ouput_$f\_$a -i $num_iter -q $queries\_test_$f"
+		$traj_comp -g $roat_net -t $map_matched_traj\_train_$ -e $map_matched_traj\_test_$f -c ONTRAC-FULL -r 2 -f ../data/sfo_postgis.conf -m $a -n $num_threads -o ouput_$f\_$a -i $num_iter -q $queries\_test_$f
 	done
 done
