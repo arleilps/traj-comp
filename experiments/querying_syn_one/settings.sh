294750a6a95f452ab071bd941e44d7854ec92a09
Arlei Silva <arleilps@gmail.com>
2015-08-28 21:07:04 -0700

    -

--- experiments/querying_syn_one/settings.sh
+++ experiments/querying_syn_one/settings.sh
@@ -6,12 +6,12 @@
 num_folds=1
 length_subt_vec=10
 order_vec=(2)
-delay_vec=(30 60 120 240 480 960 1920)
+delay_vec=(0 30 60 120 240 480)
 
 min_sup=0
 short_path=1000
 n_threads=8
-
+num_iter=5
 map_matched_traj='../../../data/map_matched_syn_one'
 road_net='../../../data/road_net_sfo.csv'
 conf_file='../../../data/sfo_postgis.conf'
--- experiments/querying_syn_two/settings.sh
+++ experiments/querying_syn_two/settings.sh
@@ -11,7 +11,7 @@ delay_vec=(30 60 120 240 480 960 1920)
 min_sup=0
 short_path=1000
 n_threads=8
-
+num_iter=5
 map_matched_traj='../../../data/map_matched_syn_two'
 road_net='../../../data/road_net_sfo.csv'
 conf_file='../../../data/sfo_postgis.conf'
