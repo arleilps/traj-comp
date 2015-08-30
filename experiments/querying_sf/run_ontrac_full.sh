2465330bb8589dbb69fbb02f95b70093f4be1c91
Arlei Silva <arleilps@gmail.com>
2015-08-29 18:57:28 -0700

    -

--- experiments/querying_sf/run_ontrac_full.sh
+++ experiments/querying_sf/run_ontrac_full.sh
@@ -13,8 +13,5 @@ do
 	do
 		echo "$traj_comp -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_test_$f -c ONTRAC-FULL -r 2 -f $conf_file -m $a -n $n_threads -o output_$f\_$a -i $num_iter -q $queries\_test_$f >> ONTRACFULL/result_$f\_$a"
 		$traj_comp -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_test_$f -c ONTRAC-FULL -r 2 -f $conf_file -m $a -n $n_threads -o output_$f\_$a -i $num_iter -q $queries\_test_$f >> ONTRACFULL/result_$f\_$a
-		
-		echo "$traj_comp -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_test_$f -c ONTRAC-PART -r 2 -f $conf_file -m $a -n $n_threads -o output_$f\_$a -i $num_iter -q $queries\_test_$f >> ONTRACPART/result_$f\_$a"
-		$traj_comp -g $road_net -t $map_matched_traj\_train_$f -e $map_matched_traj\_test_$f -c ONTRAC-PART -r 2 -f $conf_file -m $a -n $n_threads -o output_$f\_$a -i $num_iter -q $queries\_test_$f >> ONTRACPART/result_$f\_$a
 	done
 done
