d4750fcc0cd7a036053bcbd9102c45a776532aef
Arlei Silva <arleilps@gmail.com>
2015-08-31 20:19:30 -0700

    -

--- experiments/querying_sf/run_no_compression.sh
+++ experiments/querying_sf/run_no_compression.sh
@@ -8,6 +8,6 @@ mkdir NOCOMPRESSION/
 
 for ((f=1; f<=$num_folds;f++))
 do
-	echo "$traj_comp -c NONE -g $road_net -e $map_matched_traj\_test_$f -f $conf_file -o output_$f -q $queries\_test_$f > NOCOMPRESSION/results_$f"
-	$traj_comp -c NONE -g $road_net -e $map_matched_traj\_test_$f -f $conf_file -o output_$f -q $queries\_test_$f > NOCOMPRESSION/results_$f
+	echo "$traj_comp -c NONE -g $road_net -e $map_matched_traj\_test_$f -f $conf_file -o NOCOMPRESSION/output_$f -q $queries\_test_$f > NOCOMPRESSION/results_$f"
+	$traj_comp -c NONE -g $road_net -e $map_matched_traj\_test_$f -f $conf_file -o NOCOMPRESSION/output_$f -q $queries\_test_$f > NOCOMPRESSION/results_$f
 done
