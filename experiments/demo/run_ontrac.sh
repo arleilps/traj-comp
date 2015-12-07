57060ce9a4afafea50f7e9bf561585e42df15d4a
Arlei Silva <arleilps@gmail.com>
2015-12-07 13:07:33 -0800

    Fixing again.

--- experiments/demo/run_ontrac.sh
+++ experiments/demo/run_ontrac.sh
@@ -8,8 +8,8 @@ mkdir ONTRAC/
 
 for a in ${order_vec[@]}
 do
-	tfold="$trajectory_folder\_$a"
+	tfold=$trajectory_folder\_$a
 	mkdir $tfold
-	echo "$traj_comp -g $road_net -t $map_matched_traj\_train_0 -e $map_matched_traj\_test_0 -c ONTRAC-FULL -r 2 -f $conf_file -m 60 -n $n_threads -o ONTRAC/output_0 -i $num_iter -q $queries\_test_0 -j $tfold >> ONTRAC/result_0"
-	$traj_comp -g $road_net -t $map_matched_traj\_train_0 -e $map_matched_traj\_test_0 -c ONTRAC-FULL -r 2 -f $conf_file -m 60 -n $n_threads -o ONTRAC/output_0 -i $num_iter -q $queries\_test_0 -j $tfold >> ONTRAC/result_0
+	echo "$traj_comp -g $road_net -t $map_matched_traj\_train_1 -e $map_matched_traj\_test_1 -c ONTRAC-FULL -r 2 -f $conf_file -m 60 -n $n_threads -o ONTRAC/output_1 -i $num_iter -q $queries\_test_1 -j $tfold >> ONTRAC/result_1"
+	$traj_comp -g $road_net -t $map_matched_traj\_train_1 -e $map_matched_traj\_test_1 -c ONTRAC-FULL -r 2 -f $conf_file -m 60 -n $n_threads -o ONTRAC/output_1 -i $num_iter -q $queries\_test_1 -j $tfold >> ONTRAC/result_1
 done
