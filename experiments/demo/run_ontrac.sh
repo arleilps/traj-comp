091ccb362bc18866676a2612e024442c4ad305a3
Arlei Silva <arleilps@gmail.com>
2015-12-07 12:41:34 -0800

    Fixing.

--- experiments/demo/run_ontrac.sh
+++ experiments/demo/run_ontrac.sh
@@ -8,7 +8,7 @@ mkdir ONTRAC/
 
 for a in ${order_vec[@]}
 do
-	tfold="$traj_folder\_$a"
+	tfold="$trajectory_folder\_$a"
 	mkdir $tfold
 	echo "$traj_comp -g $road_net -t $map_matched_traj\_train_0 -e $map_matched_traj\_test_0 -c ONTRAC-FULL -r 2 -f $conf_file -m 60 -n $n_threads -o ONTRAC/output_0 -i $num_iter -q $queries\_test_0 -j $tfold >> ONTRAC/result_0"
 	$traj_comp -g $road_net -t $map_matched_traj\_train_0 -e $map_matched_traj\_test_0 -c ONTRAC-FULL -r 2 -f $conf_file -m 60 -n $n_threads -o ONTRAC/output_0 -i $num_iter -q $queries\_test_0 -j $tfold >> ONTRAC/result_0
